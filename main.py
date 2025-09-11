4d4bdf5c216e646855f99bf766f833ef993d6e28
Mzknkung53 <139463949+Mzknkung53@users.noreply.github.com>
2025-09-11 22:48:03 +0700

    Auto run main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(debug=False, port=5000, use_reloader=False)
+    # Enable debug and reloader for hot-reload in development
+    # app.run(debug=False, port=5000, use_reloader=False)
+    app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=True)
 
 
